fe0a24413c341c7f7d9cde51f0c065c9662d4213
fasih_ek <fasihedakkudi@gmail.com>
2024-03-13 16:31:35 +0530

    router commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./ReduxToolkit/App/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
